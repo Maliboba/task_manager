f449c8693840b27ed42beda5dc1cd766e1bd3bc3
Maliboba <claudiaagyeere@gmail.com>
2025-08-25 14:51:41 +0000

    venv created and pymongo install

--- main.py
+++ main.py
@@ -1,22 +1,4 @@
-import add
-import show
-import update
-import delete
+from oop import Chat
 
-add_task_response = add.add_task("Sleep")
-print(add_task_response)
-
-show_tasks_response = show.show_tasks()
-print(show_tasks_response)
-
-update_task_response = update.update_task("Sleep", "Wake Up")
-print(update_task_response)
-
-delete_task_response = delete.delete_task("Wake Up")
-print(delete_task_response)
-
-
-
-
-
-   
+chat_with_abena = Chat()
+print(chat_with_abena)
---
+++ main2.py
@@ -0,0 +1,5 @@
+def add(a, b):
+    result = a + b
+    return result
+yaah = add(4, 6)
+print(yeah)
