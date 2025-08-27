710c17fcf6553adc9b673973e1f51b352de2b613
Maliboba <claudiaagyeere@gmail.com>
2025-08-27 15:21:38 +0000

    show,add & delete for task manager

--- main2.py
+++
@@ -1,5 +0,0 @@
-def add(a, b):
-    result = a + b
-    return result
-yaah = add(4, 6)
-print(yeah)
--- update.py
+++
@@ -1,4 +0,0 @@
-def update_task(task, update):
-    # update task in database
-    # Return response
-    return True
